b1f341bbbe976a0ec05b863008bf3868f6969a79
bhagatajay13 <bhagatajay13@gmail.com>
2022-07-07 20:22:42 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,9 +48,9 @@ export class AppComponent {
     this.agGrid.api.deselectAll();
   }
 
-  // rowData = [
-  //   { make: 'Toyota', model: 'Celica', price: 35000 },
-  //   { make: 'Ford', model: 'Mondeo', price: 32000 },
-  //   { make: 'Porsche', model: 'Boxster', price: 72000 },
-  // ];
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 },
+  ];
 }
